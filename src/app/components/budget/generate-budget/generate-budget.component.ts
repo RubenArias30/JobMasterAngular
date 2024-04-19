4008bed3150678204989e4c8d5fccb6e25d2b03c
Arshdeepxv15 <165770495+Arshdeepxv15@users.noreply.github.com>
2024-04-19 17:43:31 +0200

    responsive sin conflictos

--- src/app/components/budget/generate-budget/generate-budget.component.ts
+++ src/app/components/budget/generate-budget/generate-budget.component.ts
@@ -9,84 +9,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./generate-budget.component.css']
 })
 export class GenerateBudgetComponent {
-  budgetForm: FormGroup;
 
-<<<<<<< HEAD
-  constructor
-  (private apiService: ApiService,
-  private router: Router,
 
-  ) { }
-=======
+
   constructor(private apiService: ApiService, private router: Router, private fb: FormBuilder) {
     // Inicializa el formulario y define las reglas de validación
-    this.budgetForm = this.fb.group({
-      client_name: ['', Validators.required],
-      client_telephone: ['', Validators.required],
-      client_email: ['', [Validators.required, Validators.email]],
-      client_nif: ['', Validators.required],
->>>>>>> cdbb060f366eddc154967bca368fb1dbb3867302
 
-      company_name: ['', Validators.required],
-      company_telephone: ['', Validators.required],
-      company_email: ['', [Validators.required, Validators.email]],
-      company_nif: ['', Validators.required],
-
-      street: ['', Validators.required],
-      city: ['', Validators.required],
-      postal_code: ['', Validators.required],
-
-      concepts: this.fb.array([]) // No se inicializa con ningún concepto
-    });
-
-    // Agregar un concepto por defecto al cargar el componente
-    this.addConcept();
-  }
-
-  // Método para agregar un concepto al formulario
-  addConcept(): void {
-    const concepts = this.budgetForm.get('concepts') as FormArray;
-    concepts.push(this.fb.group({
-      concept: ['', Validators.required],
-      price: ['', Validators.required],
-      quantity: ['', Validators.required],
-      concept_discount: ['', Validators.required],
-      concept_iva: ['', Validators.required],
-      concept_irpf: ['', Validators.required],
-    }));
   }
 
 
-  // Método para eliminar un concepto del formulario
-  removeConcept(index: number): void {
-    const concepts = this.budgetForm.get('concepts') as FormArray;
-    concepts.removeAt(index);
-  }
+
+
 
   // Método para enviar el presupuesto al servidor
-  addBudget(): void {
-    console.log(this.budgetForm.value)
 
-    // Obtén los datos del formulario principal (datos del presupuesto)
-    const budgetData = this.budgetForm.value;
 
-    // Obtén los datos de los conceptos del formulario y agrégalos a los datos del presupuesto
-    const concepts = this.budgetForm.get('concepts')!;
-    budgetData.concepts = concepts ? concepts.value : [];
-
-    // Llama al método del servicio API para crear el presupuesto con todos los datos
-    this.apiService.createInvoice(budgetData).subscribe(
-      (response) => {
-        console.log('Presupuesto creado exitosamente:', response);
-        this.router.navigate(['/budget']);
-      },
-      (error) => {
-        console.error('Error al crear el presupuesto:', error);
-        // Podrías manejar el error aquí
-      }
-    );
-  }
-<<<<<<< HEAD
   cancelEdit(): void {
     if (confirm('¿Estás seguro de cancelar la edición?')) {
       // Si el usuario confirma la cancelación, redirige a la página de administración de empleados
@@ -94,11 +31,8 @@ export class GenerateBudgetComponent {
     }
   }
 
-=======
 
-  // Método para obtener los controles de los conceptos
-  get conceptsControls() {
-    return (this.budgetForm.get('concepts') as FormArray)?.controls;
+
+
   }
->>>>>>> cdbb060f366eddc154967bca368fb1dbb3867302
-}
+
