e6cf205b1465f4a10a9784ae06ea123f25d6af2a
Arshdeepxv15 <165770495+Arshdeepxv15@users.noreply.github.com>
2024-05-03 14:46:59 +0200

    add absence

--- src/app/components/absences/absences.component.ts
+++ src/app/components/absences/absences.component.ts
@@ -1,36 +1,38 @@
 import { Component,OnInit  } from '@angular/core';
 import { ApiService } from 'src/app/services/api/api.service';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-
-
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-ausencias',
-  templateUrl: './ausencias.component.html',
-  styleUrls: ['./ausencias.component.css']
+  selector: 'app-absences',
+  templateUrl: './absences.component.html',
+  styleUrls: ['./absences.component.css']
 })
-export class AusenciasComponent {
+export class AbsencesComponent {
   employees: any[] = []; // Add this line
   ausenciaForm: FormGroup = this.formBuilder.group({});
 
-  constructor(private apiService: ApiService, private formBuilder: FormBuilder)  {}
+  constructor(private apiService: ApiService, private formBuilder: FormBuilder)  {
+
+    this.ausenciaForm = this.formBuilder.group({
+      // name: ['', Validators.required],
+      employee_id: ['', Validators.required], // Updated form control name
+      type_absence: ['', Validators.required],
+      start_date: ['', [Validators.required, this.validateStartDate]],
+      end_date: ['', [Validators.required, this.validateEndDate]],
+      motive: ['', Validators.required]
+
+      // Add other form controls here
+    });
+
+  }
 
   ngOnInit(): void {
     this.apiService.getEmployees().subscribe((employees) => {
       this.employees = employees;
 
-
-      this.ausenciaForm = this.formBuilder.group({
-        name: ['', Validators.required],
-        absenceType: ['', Validators.required],
-        startDate: ['', [Validators.required, this.validateStartDate]],
-        endDate: ['', [Validators.required, this.validateEndDate]],
-        description: ['', Validators.required]
-
-
-        // Add other form controls here
       });
-      });
+
   }
   validateStartDate(control: FormControl): { [key: string]: any } | null {
     const selectedDate = new Date(control.value);
@@ -44,7 +46,6 @@ export class AusenciasComponent {
     }
     return null;
   }
-
 
   validateEndDate(control: FormControl): { [key: string]: any } | null {
     const selectedDate = new Date(control.value);
@@ -68,17 +69,25 @@ export class AusenciasComponent {
     this.showModal = false;
   }
 
-  isSwitchLeft = true; // Assuming this is your initial state for the switch
-  selectedOption: string = 'Medio día'; // Set the initial selected option here
-
-  toggleSwitch(option: string) {
-    this.selectedOption = option;
-    this.isSwitchLeft = !this.isSwitchLeft;
-  }
-
   onSubmit(): void {
-    // Add your form submission logic here
+    // Log the form data before submitting
+    console.log('Form Data:', this.ausenciaForm.value);
+
+    // Call the addAbsence method of ApiService with form data
+    this.apiService.addAbsence(this.ausenciaForm.value).subscribe({
+      next: (response) => {
+        console.log('Response from server:', response);
+        // Optionally handle the response here
+      },
+      error: (error) => {
+        console.error('Error:', error);
+        // Optionally handle the error here
+      }
+    });
   }
+
+
+
 
 
 }
---
+++ src/app/components/absences/add-absences/add-absences.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-absences',
+  templateUrl: './add-absences.component.html',
+  styleUrls: ['./add-absences.component.css']
+})
+export class AddAbsencesComponent {
+
+}
