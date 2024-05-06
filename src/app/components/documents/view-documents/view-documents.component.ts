4da2eaceb6a4d01471839bf0662f988b134e358d
Ruben <rubenangelarias30@gmail.com>
2024-05-06 13:51:05 +0200

    view documents

---
+++ src/app/components/documents/view-documents/view-documents.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api/api.service';
+import { Document } from '../../../models/documents/documents.model';
+
+@Component({
+  selector: 'app-view-documents',
+  templateUrl: './view-documents.component.html',
+  styleUrls: ['./view-documents.component.css']
+})
+export class ViewDocumentsComponent implements OnInit {
+
+  documents: Document[] = [];
+  documentTypes: string[] = ['contracts', 'nif', 'curriculum', 'laboral_life', 'payroll', 'proof'];
+  selectedType: string = '';
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.loadDocuments();
+  }
+
+  loadDocuments() {
+    this.apiService.getMyDocuments().subscribe(
+      (response: Document[]) => {
+        this.documents = response;
+        // Aplicar filtro si hay un tipo seleccionado
+        if (this.selectedType !== '') {
+          this.documents = this.documents.filter(document => document.type_documents === this.selectedType);
+        }
+        console.log(response);
+      },
+      (error) => {
+        console.error('Error al cargar los documentos:', error);
+      }
+    );
+  }
+
+  filterDocumentsByType(type: string) {
+    // Actualizar el tipo seleccionado y volver a cargar los documentos
+    this.selectedType = type;
+    this.loadDocuments();
+  }
+}
