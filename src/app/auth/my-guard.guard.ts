517fc360e310bfd36cc9c3842e530a237813f3c8
Ruben <rubenangelarias30@gmail.com>
2024-05-22 08:51:16 +0200

    guards documentation

--- src/app/auth/my-guard.guard.ts
+++ src/app/auth/my-guard.guard.ts
@@ -6,21 +6,37 @@ import { AuthService } from '../services/auth/auth.service';
   providedIn: 'root'
 })
 export class MyGuardGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
 
+  /**
+    * MyGuardGuard class constructor.
+    *
+    * @param authService - Authentication service to check the user's authentication status.
+    * @param router - Routing service to navigate to different routes.
+    */
+  constructor(private authService: AuthService, private router: Router) { }
+
+  /**
+     * canActivate method that determines whether a route can be activated or not.
+     *
+     * @param route - Snapshot of the active route containing information about the current route.
+     * @returns boolean - Returns true if the user can activate the route, false otherwise.
+     */
   canActivate(route: ActivatedRouteSnapshot): boolean {
+    // Check if the user is authenticated
     if (this.authService.isAuthenticated()) {
+      // Get the role of the authenticated user
       const userRole = this.authService.getUserRole();
+    // Check if the route has allowed roles and if the user's role is within those roles
       if (route.data['roles'] && route.data['roles'].includes(userRole)) {
-        // Si el rol del usuario está permitido para esta ruta, permite el acceso
+        // If the user's role is allowed for this route, allow access
         return true;
       } else {
-        // Si el rol del usuario no está permitido para esta ruta, redirige a una página de acceso denegado
+        // If the user's role is not allowed for this route, display an alert and deny access
         alert('No tienes permisos para esta ruta');
         return false;
       }
     } else {
-      // Si el usuario no está autenticado, redirige a la página de inicio de sesión
+     // If the user is not authenticated, redirect to the login pages
       this.router.navigate(['/login']);
       return false;
     }
