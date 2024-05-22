9ea8083d699e0270a2d5cdb2b758a5cb33107810
DanielRubiesSmx2a <93671926+DanielRubiesSmx2a@users.noreply.github.com>
2024-05-22 15:07:36 -0400

    fichaje color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,12 @@ module.exports = {
     extend: {
       colors: {
         custom: {
-          500: '#00DEA3', // Agregando el color personalizado
+          400: '#E9F9E9', 
+          500: '#00DEA3', 
           600: '#DE8500',
           700: '#EFF3FD',
-          800: '#92E3A9'
+          800: '#92E3A9',
+          
         },
       },
     },
