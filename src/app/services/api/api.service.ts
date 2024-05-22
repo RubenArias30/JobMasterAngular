4c7a00b4b67b29a55dd4398968af3a3b5c170de2
ruben <158273921+RubenArias30@users.noreply.github.com>
2024-05-22 17:02:07 +0200

    absences model

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -5,6 +5,7 @@ import { catchError, tap } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
 import { Event } from '../../models/event.model';
 import { Employee } from 'src/app/models/employee.model';
+import { Absence } from 'src/app/models/absence.model';
 
 @Injectable({
   providedIn: 'root'
@@ -177,7 +178,7 @@ downloadDocument(documentId: number): Observable<Blob> {
 
 
   //Ausencias
-  getAusencias(): Observable<any[]> {
+  getAusencias(): Observable<Absence[]> {
     return this.http.get<any[]>(`${this.apiUrl}/absences`);
   }
 
